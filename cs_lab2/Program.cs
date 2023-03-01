551856777e9ac720038960dedc67ec8dc334d5b4
Нияз <@MSI>
2023-03-01 14:11:40 +0300

    Добавьте файлы проекта.

---
+++ cs_lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+
+Console.Write("Введите количество людей: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Person[] person = new Person[n];
+for (int i = 0; i < n; i++)
+{
+    person[i] = new Person();
+    person[i].input();
+}
+foreach (Person p in person)
+{
+    Console.WriteLine($"Имя: {p.SurName}.{p.Name[0]}. Возраст: {p.age()} Пол: {p.Sex}");
+}
